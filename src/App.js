e0f66b13c1e4ec4e8c304fa7af03e3d05b3b9561
krupali pilgulwar <rpkrupali@gmail.com>
2022-04-13 07:39:06 -0500

    basic header page created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <main>
+        <Header />
+      </main>
+    </div>
+  );
 }
 
 export default App;
