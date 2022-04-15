d85576c1196c1e773579333e694471133117a2d2
krupali pilgulwar <rpkrupali@gmail.com>
2022-04-14 19:28:23 -0500

    added portfolio details

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,44 +1,60 @@
 import React from "react";
+import Project from "../Project";
 
 const projects = [
   {
     name: "Photo Port",
     techStack: "React",
     git: "https://github.com/rpkrupali1/photo-port",
+    image: "PhotoPort.PNG",
   },
   {
     name: "Pizza Hunt",
     techStack: "NoSQL-MongoDb",
     git: "https://github.com/rpkrupali1/pizza-hunt",
+    image: "PizzaHunt.PNG",
   },
   {
     name: "Kitchen Around You",
     techStack: "Mysql2",
     git: "https://github.com/rpkrupali1/kitchen-around-you",
+    image: "KitchenAroundYou.PNG",
   },
   {
     name: "Note Taker",
     techStack: "ExpressJs",
     git: "https://github.com/rpkrupali1/kp-note-taker",
+    image: "NoteTaker.PNG",
   },
   {
     name: "News For You",
     techStack: "JS/Server side APIs",
     git: "https://rpkrupali1.github.io/News4U/",
+    image: "",
   },
   {
     name: "RunBuddy",
     techStack: "HTML/CSS",
     git: "https://rpkrupali1.github.io/run-buddy/",
+    image: "RunBuddy.JPG",
   },
 ];
 
 const Portfolio = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h3>{projects[0].name}</h3>
+    <div>
+      <h1>Portfolio</h1>
+      <div className="container">
+        <div className="row">
+          {projects.map((project) => (
+            <Project
+              name={project.name}
+              techStack={project.techStack}
+              git={project.git}
+              image={project.image}
+            />
+          ))}
+          <Project />
         </div>
       </div>
     </div>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-const Project = () => {};
+const Project = ({ name, techStack, git, image }) => {
+  return (
+    <div
+      className="col"
+      style={{ backgroundImage: `url(../../assets/${image})` }}
+    >
+      <h3>{name}</h3>
+      <h4>{techStack}</h4>
+      <a href={git}>{git}</a>
+    </div>
+  );
+};
 
 export default Project;
